84cc933ef6cb8dd0f064cd78a1204247fa22637e
Kieran W <alarmcuddly@outlook.com>
2019-03-25 11:00:33 +0000

    Uploaded code

---
+++ Python.ScrapeWithCookies.py
@@ -0,0 +1,72 @@
+'''
+Kieran W 2019
+
+Web scraper with cookies. Take the cookie from one page and use it to access
+another 
+'''
+# Run the program in 'debug' mode 
+DEBUG = True
+# Web page to scrape 
+START_PAGE = ''
+# Web page to submit answer to
+END_PAGE = ''
+
+'''
+Set up local-lib
+Comment this out if you have installed requests onto your system
+'''
+import sys, os
+libPath = os.path.join(os.getcwd(),'local-lib')
+sys.path.append(libPath)
+if(DEBUG):
+    for path in sys.path:
+        print(path)
+if(not DEBUG):
+    import warnings
+    warnings.filterwarnings("ignore")
+
+    
+'''
+Start web session and get the cookie 
+''' 
+import requests
+session = requests.Session()
+startPage = session.get(START_PAGE)
+
+pageContents = startPage.text
+cookie = session.cookies.get_dict()
+if(DEBUG):
+    print(pageContents)
+    print(cookie)
+
+
+'''
+Scrape the page 
+'''
+from html.parser import HTMLParser
+usefulData = []
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        if(DEBUG):
+            print("<>:", tag)
+    def handle_endtag(self, tag):
+        if(DEBUG):
+            print("</>:", tag)
+
+    def handle_data(self, data):
+        if(DEBUG):
+            print("Data:", data)
+        usefulData.append( data)          
+parser = MyHTMLParser()
+parser.feed(str(pageContents))
+
+if(DEBUG):
+    print (usefulData)
+
+'''
+Post to another url and pass the cookie 
+'''
+endPage = requests.post(END_PAGE, cookies=cookie)
+if(DEBUG):
+    print(endPage.text)
+
